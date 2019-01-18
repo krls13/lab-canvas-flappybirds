060beb0da81754f3ecf5c75d0f016d29dc8d3785
krls13 <krlsalberto13@hotmail.com>
2019-01-18 18:51:53 +0100

    iteration 1

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,18 +3,37 @@ window.onload = function() {
     startGame();
   };
 
+  var frame = 0
+
   function startGame() {
 
+    // setInterval(function() {
+    //   if (frame == 801){
+    //     frame = 0
+    //   }
+    //   drawBackground()
+    //   frame += 1
+    // }, 10)
+
     drawBackground()
+    
   }
 
   function drawBackground(){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     
-    var background = new Image();   // Create new background element
-    background.src = 'images/bg.png'; // Set source path
+    var background1 = new Image();   // Create new background element
+    background1.src = 'images/bg.png'; // Set source path
 
-    ctx.drawImage(background, 0, 0);
+    var background2 = new Image();   // Create new background element
+    background2.src = 'images/bg.png'; // Set source path
+
+    ctx.clearRect(0, 0, 800, 550);
+    ctx.drawImage(background1, frame, 0);
+    ctx.drawImage(background2, frame - 800, 0);
+
+    console.log("hola")
   }
+
 };
