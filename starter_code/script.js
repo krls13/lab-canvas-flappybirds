f4cf1ce6741e24240972e878ddd9d77a67a073ff
krls13 <krlsalberto13@hotmail.com>
2019-01-18 18:00:40 +0100

    iteration 1

--- starter_code/script.js
+++ starter_code/script.js
@@ -5,6 +5,16 @@ window.onload = function() {
 
   function startGame() {
 
+    drawBackground()
   }
 
+  function drawBackground(){
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    
+    var background = new Image();   // Create new background element
+    background.src = 'images/bg.png'; // Set source path
+
+    ctx.drawImage(background, 0, 0);
+  }
 };
