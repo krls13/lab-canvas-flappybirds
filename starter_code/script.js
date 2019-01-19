57e4a033fac00caf5effdc2f61a530444e4d7aec
krls13 <krlsalberto13@hotmail.com>
2019-01-19 15:35:56 +0100

    or

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,35 +3,82 @@ window.onload = function() {
     startGame();
   };
 
-  var frame = 0
+  function Player() {
+    this.width = 50
+    this.height = 50
+    this.playerX = 200
+    this.playerY = 275
+    this.speedX 
+    this.speedY
+    this.gravity
+    this.gravitySpeed
+  }
+
+  var player = new Player();
+  
+  var backgroundCoords = 0
+  
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
 
   function startGame() {
 
     setInterval(function() {
-      if (frame == 801){
-        frame = 0
+      if (backgroundCoords == -800){
+        backgroundCoords = 0
       }
+      ctx.clearRect(0, 0, 800, 550);
+
       drawBackground()
-      frame += 1
+      drawPlayer()
+      backgroundCoords -= 1
     }, 10)
-    
   }
 
   function drawBackground(){
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    
     var background1 = new Image();   // Create new background element
     background1.src = 'images/bg.png'; // Set source path
 
     var background2 = new Image();   // Create new background element
     background2.src = 'images/bg.png'; // Set source path
 
-    ctx.clearRect(0, 0, 800, 550);
-    ctx.drawImage(background1, frame, 0);
-    ctx.drawImage(background2, frame - 800, 0);
+    var obstacleTop1 = new Image();   // Create new background element
+    obstacleTop1.src = 'images/obstacle_top.png'; // Set source path
 
-    console.log("hola")
+    var obstacleBottom1 = new Image();   // Create new background element
+    obstacleBottom1.src = 'images/obstacle_bottom.png'; // Set source path
+
+    var obstacleTop2 = new Image();   // Create new background element
+    obstacleTop2.src = 'images/obstacle_top.png'; // Set source path
+
+    var obstacleBottom2 = new Image();   // Create new background element
+    obstacleBottom2.src = 'images/obstacle_bottom.png'; // Set source path
+
+    ctx.drawImage(background1, backgroundCoords + 800, 0);
+    ctx.drawImage(obstacleTop1, backgroundCoords + 800, 0, 60, 190);
+    ctx.drawImage(obstacleBottom1, backgroundCoords + 800, 315, 60, 190);
+
+    ctx.drawImage(background2, backgroundCoords, 0);
+    ctx.drawImage(obstacleTop2, backgroundCoords + 800, 0, 60, 190);
+    ctx.drawImage(obstacleBottom2, backgroundCoords + 800, 315, 60, 190);
+
   }
 
+  function drawPlayer() {
+    var player_image = new Image()
+    player_image.src= "images/flappy.png"
+
+    ctx.drawImage(player_image, player.playerX, player.playerY, player.width, player.height)
+  }
+  
+  document.addEventListener("keydown", function(e){
+    
+  if(e.keyCode == 32){
+      player.playerY -=8
+      console.log("XXX");
+    }
+  else{ player.playerY +=10
+  }
+
+  })
 };
